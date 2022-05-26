4e4daa8fd234fb8fbd46f0db03eb5c0acf2eeeb0
som <somsinghlodhi2323@gmail.com>
2022-05-26 11:26:03 +0530

    basic code for stack using  array:

--- create_stack.cpp
+++ create_stack.cpp
@@ -13,6 +13,7 @@ class Stack{
     */
 };// stack using an array:_)
 void create(Stack &obj , int size){
+    obj.size = size ;
     obj.p = new int[size];
     obj.top = -1;
      
@@ -21,11 +22,11 @@ void display(Stack &obj ){
    for(int i = obj.top ; i>=0 ; i--)cout<<obj.p[i]<<" ";cout<<endl;
 }
 void push( Stack *ptr, int x){
-    if(ptr->size==(ptr->top-1)){
+    if(ptr->top>=ptr->size-1){
         cout<<"Stack is Full"<<endl;
     }
     else {
-        ptr->top++;
+        (ptr->top)+=1;
         ptr->p[ptr->top] = x;
 
     }
@@ -43,9 +44,21 @@ int pop( Stack &ptr){
         return x ;
     }
 }
+void    isFull(Stack *ptr){if(ptr->size == ptr->top+1){
+cout<<"true"<<endl;
+    }
+    else {
+        cout<<"false"<<endl;
+        
+    }
+}
 int main(){
 Stack obj1;
 create(obj1,4);
-push(&obj1 , 40);push(&obj1 , 41);push(&obj1 , 402);push(&obj1 , 404);
+push(&obj1 , 40);push(&obj1 , 41);push(&obj1 , 402);push(&obj1 , 404);isFull(&obj1);
 display(obj1);
+pop(obj1);
+display(obj1);
+cout<<obj1.size<<endl;
+isFull(&obj1);
 }
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,8 @@
+void    isFull(Stack *ptr){if(ptr->size == ptr->top+1){
+// cout<<"true"<<endl;
+//     }
+//     else {
+//         cout<<"false"<<endl;
+        
+//     }
+// }
