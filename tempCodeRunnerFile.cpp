55a6b484d9ae4b1abf74b72537a52e79ca2acf4b
som <somsinghlodhi2323@gmail.com>
2022-05-29 14:59:30 +0530

    done with parenthesis_matching.cpp :)

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,8 +1,1 @@
-void    isFull(Stack *ptr){if(ptr->size == ptr->top+1){
-// cout<<"true"<<endl;
-//     }
-//     else {
-//         cout<<"false"<<endl;
-        
-//     }
-// }
+para_match(objectone , a);
